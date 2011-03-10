04194e73286f2830aeaa068f83726e42b4ec7d8f
Greg Haines <gresrun@gmail.com>
2011-03-10 00:07:37 -0500

    Added an InfiniteAction for testing purposes.

---
+++ src/test/java/net/greghaines/jesque/InfiniteAction.java
@@ -0,0 +1,17 @@
+package net.greghaines.jesque;
+
+public class InfiniteAction implements Runnable
+{
+	public InfiniteAction(){}
+
+	public void run()
+	{
+		while (true)
+		{
+			synchronized (this)
+			{
+				try { this.wait(); } catch (Exception e){}
+			}
+		}
+	}
+}
