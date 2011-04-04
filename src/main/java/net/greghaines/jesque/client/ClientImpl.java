19989fb3374d560c0f6f467bb3ce8c627c38529b
Greg Haines <gresrun@gmail.com>
2011-04-04 19:57:56 -0400

    A more robust approach to connection management.

--- src/main/java/net/greghaines/jesque/client/ClientImpl.java
+++ src/main/java/net/greghaines/jesque/client/ClientImpl.java
@@ -15,6 +15,10 @@
  */
 package net.greghaines.jesque.client;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import net.greghaines.jesque.Config;
 import net.greghaines.jesque.utils.JedisUtils;
 
@@ -26,16 +30,32 @@ import redis.clients.jedis.Jedis;
  * @author Greg Haines
  */
 public class ClientImpl extends AbstractClient
-{	
+{
+	public static final boolean DEFAULT_CHECK_CONNECTION_BEFORE_USE = false;
+	
 	private final Jedis jedis;
+	private final boolean checkConnectionBeforeUse;
+	private final ScheduledExecutorService keepAliveService;
+
+	/**
+	 * Create a new ClientImpl, which creates it's own connection to Redis using values from the config.
+	 * It will not verify the connection before use.
+	 * 
+	 * @param config used to create a connection to Redis
+	 */
+	public ClientImpl(final Config config)
+	{
+		this(config, DEFAULT_CHECK_CONNECTION_BEFORE_USE);
+	}
 	
 	/**
 	 * Create a new ClientImpl, which creates it's own connection to Redis using values from the config.
 	 * 
 	 * @param config used to create a connection to Redis
+	 * @param checkConnectionBeforeUse check to make sure the connection is alive before using it
 	 * @throws IllegalArgumentException if the config is null
 	 */
-	public ClientImpl(final Config config)
+	public ClientImpl(final Config config, final boolean checkConnectionBeforeUse)
 	{
 		super(config);
 		this.jedis = new Jedis(config.getHost(), config.getPort(), config.getTimeout());
@@ -44,17 +64,55 @@ public class ClientImpl extends AbstractClient
 			this.jedis.auth(config.getPassword());
 		}
 		this.jedis.select(config.getDatabase());
+		this.checkConnectionBeforeUse = checkConnectionBeforeUse;
+		this.keepAliveService = null;
+	}
+
+	/**
+	 * Create a new ClientImpl, which creates it's own connection to Redis using values from the config and 
+	 * spawns a thread to ensure the connection stays open.
+	 * 
+	 * @param config used to create a connection to Redis
+	 * @param initialDelay the time to delay first connection check
+	 * @param period the period between successive connection checks
+	 * @param timeUnit the time unit of the initialDelay and period parameters
+	 */
+	public ClientImpl(final Config config, final long initialDelay, final long period, final TimeUnit timeUnit)
+	{
+		super(config);
+		this.jedis = new Jedis(config.getHost(), config.getPort(), config.getTimeout());
+		if (config.getPassword() != null)
+		{
+			this.jedis.auth(config.getPassword());
+		}
+		this.jedis.select(config.getDatabase());
+		this.checkConnectionBeforeUse = false;
+		this.keepAliveService = Executors.newSingleThreadScheduledExecutor();
+		this.keepAliveService.scheduleAtFixedRate(new Runnable()
+		{
+			public void run()
+			{
+				JedisUtils.ensureJedisConnection(ClientImpl.this.jedis);
+			}
+		}, initialDelay, period, timeUnit);
 	}
 	
 	@Override
 	protected void doEnqueue(final String queue, final String jobJson)
 	{
-		JedisUtils.ensureJedisConnection(this.jedis);
+		if (this.checkConnectionBeforeUse)
+		{
+			JedisUtils.ensureJedisConnection(this.jedis);
+		}
 		doEnqueue(this.jedis, getNamespace(), queue, jobJson);
 	}
 	
 	public void end()
 	{
+		if (this.keepAliveService != null)
+		{
+			this.keepAliveService.shutdownNow();
+		}
 		this.jedis.quit();
 	}
 }
