1bf4d4af0e1b3fad752d5a5c514bff12564f4443
Greg Haines <gresun@gmail.com>
2012-12-04 22:11:35 -0500

    WIP for WorkerPool example test

--- src/test/java/net/greghaines/jesque/AdminIntegrationTest.java
+++ src/test/java/net/greghaines/jesque/AdminIntegrationTest.java
@@ -3,19 +3,26 @@ package net.greghaines.jesque;
 import static net.greghaines.jesque.utils.JesqueUtils.entry;
 import static net.greghaines.jesque.utils.JesqueUtils.map;
 import static net.greghaines.jesque.utils.JesqueUtils.set;
+
+import java.util.concurrent.Callable;
+
 import net.greghaines.jesque.admin.Admin;
 import net.greghaines.jesque.admin.AdminClient;
 import net.greghaines.jesque.admin.AdminClientImpl;
 import net.greghaines.jesque.admin.AdminImpl;
 import net.greghaines.jesque.worker.Worker;
 import net.greghaines.jesque.worker.WorkerImpl;
+import net.greghaines.jesque.worker.WorkerPool;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AdminIntegrationTest
 {
+	private static final Logger log = LoggerFactory.getLogger(AdminIntegrationTest.class);
 	private static final Config config = new ConfigBuilder().build();
 	private static final String testQueue = "foo";
 	
@@ -25,9 +32,45 @@ public class AdminIntegrationTest
 		TestUtils.resetRedis(config);
 	}
 	
+	@Test
+	public void testAdminAndWorkerPool()
+	{
+		final WorkerPool workerPool = new WorkerPool(new Callable<WorkerImpl>(){
+			@SuppressWarnings("unchecked")
+			public WorkerImpl call()
+			{
+				return new WorkerImpl(config, set(testQueue), map(entry("TestAction", TestAction.class)));
+			}
+		}, 2);
+		final Admin admin = new AdminImpl(config);
+		admin.setWorker(workerPool);
+		
+		workerPool.run();
+		final Thread adminThread = new Thread(admin);
+		adminThread.start();
+		
+		Assert.assertFalse(workerPool.isPaused());
+		
+		try
+		{
+			// TODO: Do client stuff here
+		}
+		finally
+		{
+			TestUtils.stopWorker(admin, adminThread);
+			try
+			{
+				workerPool.endAndJoin(false, 1000);
+			}
+			catch (Exception e){
+				log.warn("Exception while waiting for workerThread to join", e);
+			}
+		}
+	}
+	
 	@SuppressWarnings("unchecked")
 	@Test
-	public void testPauseCommand()
+	public void testPauseAndShutdownCommands()
 	{
 		final Worker worker = new WorkerImpl(config, set(testQueue), map(entry("TestAction", TestAction.class)));
 		final Admin admin = new AdminImpl(config);
