007555d3f3b83bd1cf190d175cba1785e837629d
Greg Haines <gresun@gmail.com>
2013-02-16 11:40:47 -0500

    A better test for Issue #28

--- src/test/java/net/greghaines/jesque/LongRunningTest.java
+++ src/test/java/net/greghaines/jesque/LongRunningTest.java
@@ -1,35 +1,47 @@
 package net.greghaines.jesque;
-
+ 
 import static net.greghaines.jesque.utils.JesqueUtils.entry;
 import static net.greghaines.jesque.utils.JesqueUtils.map;
-
+ 
 import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicBoolean;
-
+ 
 import net.greghaines.jesque.worker.Worker;
 import net.greghaines.jesque.worker.WorkerEvent;
 import net.greghaines.jesque.worker.WorkerImpl;
 import net.greghaines.jesque.worker.WorkerListener;
-
+ 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import redis.clients.jedis.Jedis;
+ 
 public class LongRunningTest
 {
 	private static final Logger log = LoggerFactory.getLogger(LongRunningTest.class);
 	private static final Config config = new ConfigBuilder().build();
 	
-	private static void sleepTight(final long millis) {
-		try {
+	private static void sleepTight(final long millis)
+	{
+		try
+		{
 			Thread.sleep(millis);
-		} catch (InterruptedException e) {
+		}
+		catch (InterruptedException e)
+		{
 			e.printStackTrace();
 		}
 	}
-
+	
+	private static void changeRedisTimeout(final long seconds)
+	{
+		final Jedis jedis = TestUtils.createJedis(config);
+    	jedis.configSet("timeout", Long.toString(seconds));
+    	jedis.disconnect();	
+	}
+ 
 	@Before
 	public void resetRedis()
 	throws Exception
@@ -45,16 +57,17 @@ public class LongRunningTest
 	public void issue28()
 	throws InterruptedException
 	{
-		TestUtils.enqueueJobs("longRunning", Arrays.asList(new Job("LongRunningAction")), config);
-		final Worker worker2 = new WorkerImpl(config, Arrays.asList("longRunning"), 
+		changeRedisTimeout(10);
+		TestUtils.enqueueJobs("longRunning", Arrays.asList(new Job("LongRunningAction", 20*1000L)), config);
+		final Worker worker2 = new WorkerImpl(config, Arrays.asList("longRunning"),
 			map(entry("LongRunningAction", LongRunningAction.class)));
-		final AtomicBoolean succesCallbackInvokedRef = new AtomicBoolean(false);
+		final AtomicBoolean successRef = new AtomicBoolean(false);
 		worker2.addListener(new WorkerListener() {
 			@Override
 			public void onEvent(final WorkerEvent event, final Worker worker, final String queue,
 					final Job job, final Object runner, final Object result, final Exception ex) {
-				succesCallbackInvokedRef.set(true);
-				log.info("SUCCCES- {}", job);
+				successRef.set(true);
+				log.info("SUCCCESS: {}", job);
 			}
 		}, WorkerEvent.JOB_SUCCESS);
 		final Thread workerThread2 = new Thread(worker2);
@@ -62,16 +75,23 @@ public class LongRunningTest
 		sleepTight(1000);
 		worker2.end(false);
 		workerThread2.join();
-		Assert.assertTrue("Success callback should have been called", succesCallbackInvokedRef.get());
+ 
+    	Assert.assertTrue("Success callback should have been called", successRef.get());
+		changeRedisTimeout(0);
 	}
 	
 	public static class LongRunningAction implements Runnable
 	{
-		public LongRunningAction(){}
+		private final int millis;
+		
+		public LongRunningAction(final int millis)
+		{
+			this.millis = millis;
+		}
 
 		public void run()
 		{
-			sleepTight(10*60*1000); // Sleep for 10 minutes
+			sleepTight(this.millis);
 		}
 	}
 }
