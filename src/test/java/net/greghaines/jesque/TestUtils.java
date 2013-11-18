2322c866130acbee081a1db086e4b6a6bc28345f
Greg Haines <gresrun@gmail.com>
2013-11-17 22:07:36 -0500

    Whitespace

--- src/test/java/net/greghaines/jesque/TestUtils.java
+++ src/test/java/net/greghaines/jesque/TestUtils.java
@@ -31,97 +31,81 @@ import redis.clients.jedis.Jedis;
  * 
  * @author Greg Haines, Animesh Kumar <smile.animesh@gmail.com>
  */
-public final class TestUtils
-{
-	private static final Logger log = LoggerFactory.getLogger(TestUtils.class);
-	
-	/**
-	 * Reset the Redis database using the supplied Config.
-	 * 
-	 * @param config the location of the Redis server
-	 */
-	public static void resetRedis(final Config config)
-	{
-		final Jedis jedis = createJedis(config);
-		try
-		{
-			log.info("Resetting Redis for next test...");
-			jedis.flushDB();
-		}
-		finally
-		{
-			jedis.quit();
-		}
-	}
+public final class TestUtils {
+    
+    private static final Logger log = LoggerFactory.getLogger(TestUtils.class);
 
-	/**
-	 * Create a connection to Redis from the given Config.
-	 * 
-	 * @param config the location of the Redis server
-	 * @return a new connection
-	 */
-	public static Jedis createJedis(final Config config)
-	{
-		if (config == null)
-		{
-			throw new IllegalArgumentException("config must not be null");
-		}
-		final Jedis jedis = new Jedis(config.getHost(), config.getPort(), config.getTimeout());
-		if (config.getPassword() != null)
-		{
-			jedis.auth(config.getPassword());
-		}
-		jedis.select(config.getDatabase());
-		return jedis;
-	}
-	
-	public static void enqueueJobs(final String queue, final List<Job> jobs, final Config config)
-	{
-		final Client client = new ClientImpl(config);
-		try
-		{
-			for (final Job job : jobs)
-			{
-				client.enqueue(queue, job);
-			}
-		}
-		finally
-		{
-			client.end();
-		}
-	}
-	
-	public static void stopWorker(final JobExecutor worker, final Thread workerThread)
-	{
-		try { Thread.sleep(1000); } catch (Exception e){} // Give worker time to process
-		worker.end(false);
-		try
-		{
-			workerThread.join();
-		}
-		catch (Exception e)
-		{
-			log.warn("Exception while waiting for workerThread to join", e);
-		}
-	}
-	
-    public static void delayEnqueueJobs(final String queue, final List<Job> jobs, final Config config)
-    {
-        final Client client = new ClientImpl(config);
-        try
-        {
-            int i = 1;
-            for (final Job job : jobs)
-            {
-                final long value = System.currentTimeMillis() + (6000 * i++);
-                client.delayedEnqueue(queue, job, value);
-            }
+    /**
+     * Reset the Redis database using the supplied Config.
+     * 
+     * @param config
+     *            the location of the Redis server
+     */
+    public static void resetRedis(final Config config) {
+        final Jedis jedis = createJedis(config);
+        try {
+            log.info("Resetting Redis for next test...");
+            jedis.flushDB();
+        } finally {
+            jedis.quit();
         }
-        finally
-        {
+    }
+
+    /**
+     * Create a connection to Redis from the given Config.
+     * 
+     * @param config
+     *            the location of the Redis server
+     * @return a new connection
+     */
+    public static Jedis createJedis(final Config config) {
+        if (config == null) {
+            throw new IllegalArgumentException("config must not be null");
+        }
+        final Jedis jedis = new Jedis(config.getHost(), config.getPort(), config.getTimeout());
+        if (config.getPassword() != null) {
+            jedis.auth(config.getPassword());
+        }
+        jedis.select(config.getDatabase());
+        return jedis;
+    }
+
+    public static void enqueueJobs(final String queue, final List<Job> jobs, final Config config) {
+        final Client client = new ClientImpl(config);
+        try {
+            for (final Job job : jobs) {
+                client.enqueue(queue, job);
+            }
+        } finally {
             client.end();
         }
     }
 
-	private TestUtils(){} // Utility class
+    public static void stopWorker(final JobExecutor worker, final Thread workerThread) {
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+        } // Give worker time to process
+        worker.end(false);
+        try {
+            workerThread.join();
+        } catch (Exception e) {
+            log.warn("Exception while waiting for workerThread to join", e);
+        }
+    }
+
+    public static void delayEnqueueJobs(final String queue, final List<Job> jobs, final Config config) {
+        final Client client = new ClientImpl(config);
+        try {
+            int i = 1;
+            for (final Job job : jobs) {
+                final long value = System.currentTimeMillis() + (500 * i++);
+                client.delayedEnqueue(queue, job, value);
+            }
+        } finally {
+            client.end();
+        }
+    }
+
+    private TestUtils() {} // Utility class
 }
