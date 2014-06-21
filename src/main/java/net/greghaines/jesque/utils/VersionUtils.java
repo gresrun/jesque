0ea5a7084cef0ad0a1d45c8548b49f9289ca8727
Greg Haines <gresrun@gmail.com>
2014-06-20 20:52:11 -0400

    Constants names and minor cleanup

--- src/main/java/net/greghaines/jesque/utils/VersionUtils.java
+++ src/main/java/net/greghaines/jesque/utils/VersionUtils.java
@@ -30,24 +30,29 @@ import org.slf4j.LoggerFactory;
  */
 public final class VersionUtils {
     
-    public static final String SNAPSHOT = "SNAPSHOT";
+    /**
+     * String for non-released versions.
+     */
     public static final String DEVELOPMENT = "DEVELOPMENT";
+    /**
+     * String shown if there was an error determining the version.
+     */
     public static final String ERROR = "ERROR";
 
-    private static final Logger log = LoggerFactory.getLogger(VersionUtils.class);
-    private static final String pomPropertiesResName = "/META-INF/maven/net.greghaines/jesque/pom.properties";
-    private static final String versionPropName = "version";
-    private static final AtomicReference<String> versionRef = new AtomicReference<String>(null);
-    private static final Pattern dotPattern = Pattern.compile("[\\.-]");
+    private static final Logger LOG = LoggerFactory.getLogger(VersionUtils.class);
+    private static final String POM_PROPERTIES_RES_NAME = "/META-INF/maven/net.greghaines/jesque/pom.properties";
+    private static final String VERSION_PROP_NAME = "version";
+    private static final AtomicReference<String> VERSION_REF = new AtomicReference<String>(null);
+    private static final Pattern DOT_PATTERN = Pattern.compile("[\\.-]");
 
     /**
      * @return the current version of this software
      */
     public static String getVersion() {
-        String version = versionRef.get();
+        String version = VERSION_REF.get();
         if (version == null) {
-            versionRef.set(readVersion());
-            version = versionRef.get();
+            VERSION_REF.set(readVersion());
+            version = VERSION_REF.get();
         }
         return version;
     }
@@ -58,10 +63,8 @@ public final class VersionUtils {
      * element is a String that is either null, for releases, or "SNAPSHOT", for
      * snapshots. <br/>
      * <br/>
-     * If this is a development build, the array is [-1, -1, -1,
-     * "DEVELOPMENT"].<br/>
-     * If there is an error determining the version, the array is [-1, -1, -1,
-     * "ERROR"].
+     * If this is a development build, the array is [-1, -1, -1, "DEVELOPMENT"].<br/>
+     * If there is an error determining the version, the array is [-1, -1, -1, "ERROR"].
      * 
      * @return the array described
      */
@@ -74,7 +77,7 @@ public final class VersionUtils {
             versionParts[2] = -1;
             versionParts[3] = version;
         } else {
-            final String[] versionStrParts = dotPattern.split(version);
+            final String[] versionStrParts = DOT_PATTERN.split(version);
             final boolean isSnapshot = (versionStrParts.length == 4);
             final int stop = isSnapshot ? versionStrParts.length - 1 : versionStrParts.length;
             for (int i = 0; i < stop; i++) {
@@ -89,19 +92,20 @@ public final class VersionUtils {
 
     private static String readVersion() {
         String version = DEVELOPMENT;
-        final InputStream stream = VersionUtils.class.getResourceAsStream(pomPropertiesResName);
+        final InputStream stream = VersionUtils.class.getResourceAsStream(POM_PROPERTIES_RES_NAME);
         if (stream != null) {
             try {
                 final Properties props = new Properties();
                 props.load(stream);
-                version = (String) props.get(versionPropName);
+                version = (String) props.get(VERSION_PROP_NAME);
             } catch (Exception e) {
-                log.warn("Could not determine version from POM properties", e);
+                LOG.warn("Could not determine version from POM properties", e);
                 version = ERROR;
             } finally {
                 try {
                     stream.close();
                 } catch (Exception e) {
+                    LOG.debug("Error closing pom properties resource stream");
                 }
             }
         }
