c6b875ea646246a58452f74a6f998486cf51f6dc
Greg Haines <gresrun@gmail.com>
2014-06-29 22:17:14 -0400

    Formatting and javadoc

--- src/main/java/net/greghaines/jesque/utils/CompositeDateFormat.java
+++ src/main/java/net/greghaines/jesque/utils/CompositeDateFormat.java
@@ -15,6 +15,7 @@
  */
 package net.greghaines.jesque.utils;
 
+import java.io.Serializable;
 import java.text.DateFormat;
 import java.text.FieldPosition;
 import java.text.ParsePosition;
@@ -24,73 +25,101 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+/**
+ * CompositeDateFormat attempts to parse dates using several known date format patterns 
+ * and formats dates using {@link ResqueDateFormatThreadLocal#getInstance()}.
+ * 
+ * @author Greg Haines
+ */
 public class CompositeDateFormat extends DateFormat {
 
-	private static final long serialVersionUID = -4079876635509458541L;
-	private static final List<DateFormatFactory> DATE_FORMAT_FACTORIES = 
-		Arrays.<DateFormatFactory>asList(
-			new DateFormatFactory() {
-				public DateFormat create() {
-					return ResqueDateFormatThreadLocal.getInstance();
-				}
-			},
-			new PatternDateFormatFactory(ResqueConstants.DATE_FORMAT_RUBY_V1),
-			new PatternDateFormatFactory(ResqueConstants.DATE_FORMAT_RUBY_V2),
-			new PatternDateFormatFactory(ResqueConstants.DATE_FORMAT_RUBY_V3),
-			new PatternDateFormatFactory(ResqueConstants.DATE_FORMAT_RUBY_V4),
-			new PatternDateFormatFactory(ResqueConstants.DATE_FORMAT_PHP)
-		);
+    private static final long serialVersionUID = -4079876635509458541L;
+    private static final List<DateFormatFactory> DATE_FORMAT_FACTORIES = Arrays.asList(
+        new DateFormatFactory() {
 
-	@Override
-	public StringBuffer format(final Date date, final StringBuffer toAppendTo,
-			final FieldPosition fieldPosition) {
-		return ResqueDateFormatThreadLocal.getInstance().format(date, toAppendTo, fieldPosition);
-	}
+            /**
+             * {@inheritDoc}
+             */
+            @Override
+            public DateFormat create() {
+                return ResqueDateFormatThreadLocal.getInstance();
+            }
+        },
+        new PatternDateFormatFactory(ResqueConstants.DATE_FORMAT_RUBY_V1),
+        new PatternDateFormatFactory(ResqueConstants.DATE_FORMAT_RUBY_V2),
+        new PatternDateFormatFactory(ResqueConstants.DATE_FORMAT_RUBY_V3),
+        new PatternDateFormatFactory(ResqueConstants.DATE_FORMAT_RUBY_V4),
+        new PatternDateFormatFactory(ResqueConstants.DATE_FORMAT_PHP)
+    );
 
-	@Override
-	public Date parse(final String dateStr, final ParsePosition pos) {
-		final ParsePosition posCopy = new ParsePosition(pos.getIndex());
-		Date date = null;
-		boolean success = false;
-		for (final DateFormatFactory dfFactory : DATE_FORMAT_FACTORIES) {
-			posCopy.setIndex(pos.getIndex());
-			posCopy.setErrorIndex(pos.getErrorIndex());
-			date = dfFactory.create().parse(dateStr, posCopy);
-			if (date != null) {
-				success = true;
-				break;
-			}
-		}
-		if (success) {
-			pos.setIndex(posCopy.getIndex());
-			pos.setErrorIndex(posCopy.getErrorIndex());
-		}
-		return date;
-	}
-	
-	private interface DateFormatFactory {
-		DateFormat create();
-	}
-	
-	private static class PatternDateFormatFactory implements DateFormatFactory {
-		private final String pattern;
-		
-		public PatternDateFormatFactory(final String pattern) {
-			this.pattern = pattern;
-		}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public StringBuffer format(final Date date, final StringBuffer toAppendTo, final FieldPosition fieldPosition) {
+        return ResqueDateFormatThreadLocal.getInstance().format(date, toAppendTo, fieldPosition);
+    }
 
-		public DateFormat create() {
-			final SimpleDateFormat dateFormat = new SimpleDateFormat(this.pattern, Locale.US);
-			dateFormat.setLenient(false);
-			return dateFormat;
-		}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Date parse(final String dateStr, final ParsePosition pos) {
+        final ParsePosition posCopy = new ParsePosition(pos.getIndex());
+        Date date = null;
+        boolean success = false;
+        for (final DateFormatFactory dfFactory : DATE_FORMAT_FACTORIES) {
+            posCopy.setIndex(pos.getIndex());
+            posCopy.setErrorIndex(pos.getErrorIndex());
+            date = dfFactory.create().parse(dateStr, posCopy);
+            if (date != null) {
+                success = true;
+                break;
+            }
+        }
+        if (success) {
+            pos.setIndex(posCopy.getIndex());
+            pos.setErrorIndex(posCopy.getErrorIndex());
+        }
+        return date;
+    }
 
-		/**
-		 * {@inheritDoc}
-		 */
+    private interface DateFormatFactory {
+        DateFormat create();
+    }
+
+    private static class PatternDateFormatFactory implements DateFormatFactory, Serializable {
+
+        private static final long serialVersionUID = 3382491374377384377L;
+
+        private final String pattern;
+
+        /**
+         * Constructor.
+         * 
+         * @param pattern
+         *            the date format pattern
+         */
+        public PatternDateFormatFactory(final String pattern) {
+            this.pattern = pattern;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public DateFormat create() {
+            final SimpleDateFormat dateFormat = new SimpleDateFormat(this.pattern, Locale.US);
+            dateFormat.setLenient(false);
+            return dateFormat;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public String toString() {
             return "PatternDateFormatFactory [pattern=" + this.pattern + "]";
         }
-	}
+    }
 }
