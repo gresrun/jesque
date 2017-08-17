2267aac0227b996d9e82c9a52e69a505b3a2a983
dvizelman <dima.vizelman@kenshoo.com>
2017-08-17 15:35:41 +0300

    extract the current time in millis to local variable

--- src/main/java/net/greghaines/jesque/worker/WorkerImpl.java
+++ src/main/java/net/greghaines/jesque/worker/WorkerImpl.java
@@ -507,14 +507,15 @@ public class WorkerImpl implements Worker {
      */
     protected String pop(final String curQueue) {
         final String key = key(QUEUE, curQueue);
-        String inFlightKey = key(INFLIGHT, this.name, curQueue);
+        final String inFlightKey = key(INFLIGHT, this.name, curQueue);
+        final String now = Long.toString(System.currentTimeMillis());
 
         switch (nextQueueStrategy) {
         case DRAIN_WHILE_MESSAGES_EXISTS:
             return (String) this.jedis.evalsha(this.popScriptHash.get(), 3, key, inFlightKey,
-                    JesqueUtils.createRecurringHashKey(key), Long.toString(System.currentTimeMillis()));
+                    JesqueUtils.createRecurringHashKey(key), now);
         case RESET_TO_HIGHEST_PRIORITY:
-            return (String) this.jedis.evalsha(this.multiPriorityQueuesScriptHash.get(), 2, curQueue, inFlightKey, Long.toString(System.currentTimeMillis()));
+            return (String) this.jedis.evalsha(this.multiPriorityQueuesScriptHash.get(), 2, curQueue, inFlightKey, now);
         default:
             throw new RuntimeException("Unimplemented 'nextQueueStrategy'");
         }
